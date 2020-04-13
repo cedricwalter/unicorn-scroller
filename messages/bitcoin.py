82f68adc1bf3a8d4211e238a25b7582b8082b2c7
Cédric Walter <cedric.walter@gmail.com>
2020-04-13 12:49:37 +0200

    add bitcoin and ethereum messages

--- messages/bitcoin.py
+++ messages/bitcoin.py
@@ -1,4 +1,6 @@
 from pycoingecko import CoinGeckoAPI
+
+# list of coins https://api.coingecko.com/api/v3/coins/list
 price = CoinGeckoAPI().get_price(ids='bitcoin', vs_currencies='usd')
 
 print 'colour:yellow'
