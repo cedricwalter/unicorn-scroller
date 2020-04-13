76cebd6d2a49c65d44eb875adc96d9b3901ee21d
Cédric Walter <cedric.walter@gmail.com>
2020-04-13 12:46:20 +0200

    add bitcoin and ethereum messages

--- messages/ethereum.py
+++ messages/ethereum.py
@@ -1,5 +1,5 @@
 from pycoingecko import CoinGeckoAPI
 price = CoinGeckoAPI().get_price(ids='ethereum', vs_currencies='usd')
 
-print 'colour:yellow'
+print 'colour:grey'
 print 'ETH ${}'.format(price['ethereum']['usd'])
